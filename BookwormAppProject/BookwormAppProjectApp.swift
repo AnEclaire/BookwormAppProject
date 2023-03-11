1b2471268c584e863fc3213a10daf933eeab795a
Emma Gutierrez <eclaire.tv@gmail.com>
2023-03-11 15:08:07 -0800

    Initial Commit

---
+++ BookwormAppProject/BookwormAppProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormAppProjectApp.swift
+//  BookwormAppProject
+//
+//  Created by Emma Gutierrez on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormAppProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
